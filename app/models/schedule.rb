01c75ed25d6c475d8d0ba4c134eee55629e7a5dc
Andre Milhoranza <amilhoranza@gmail.com>
2018-04-03 19:44:43 -0300

    Logic to be able to re-schedule appointments by justing updating schedule object

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,7 @@ require "recurrence"
 
 class Schedule < ApplicationRecord
   belongs_to :user
-  has_many :appointments
+  has_many :appointments, autosave: true
 
   validates_presence_of :starting_on, :ending_on, :note
   validates :interval, :numericality => true
@@ -14,17 +14,31 @@ class Schedule < ApplicationRecord
     :after_message => "End time cannot be before start time"
 
 
-  after_update :update_appointments
+  after_create :create_appointments
+  after_update :update_appointments, :if => :has_changed?
   after_destroy :cleanup
 
   private
 
+  def create_appointments
+    # Saving appointments for this schedule
+    recurrencies = Recurrence.new(:every => :day, :starts => starting_on, :until => ending_on, :interval => interval)
+    recurrencies.events.each do |date|
+      Appointment.create(schedule_id: id, when: "#{date} #{starting_on.strftime('%T')}", note: note)
+    end
+  end
+
   def update_appointments
-    Appointment.where(schedule_id: id, status: 'planned', manually_edited: 'no').update_all(note: note)
+    cleanup
+    create_appointments
   end
 
   def cleanup
     Appointment.where(schedule_id: id, status: 'planned', manually_edited: 'no').delete_all
+  end
+
+  def has_changed?
+    self.changed?
   end
 
 end
